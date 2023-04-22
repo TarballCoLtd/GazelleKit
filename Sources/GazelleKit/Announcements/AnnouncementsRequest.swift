102e62b3d7ca7e89780392bf8dd81ac07eb30676
Alyx Ferrari <alyx.ferrari@gmail.com>
2023-04-21 17:20:27 -0700

    debug statement

--- Sources/GazelleKit/Announcements/AnnouncementsRequest.swift
+++ Sources/GazelleKit/Announcements/AnnouncementsRequest.swift
@@ -19,6 +19,7 @@ public extension GazelleAPI {
         print(json as Any)
         #endif
         let decoder = JSONDecoder()
+        print("youre ultra fucked")
         return try Announcements(announcements: decoder.decode(RedactedAnnouncements.self, from: data), requestJson: json)
     }
 
